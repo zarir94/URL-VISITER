a4e8f5d63b61ae205a282abb29eb33b02bce0531
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2025-09-02 15:55:09 +0000

    new

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from requests import get
+import traceback
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! v3'
+    url = request.args.get('url')
+    res = dict(given=dict(url=url))
+    try:
+        r = get(url, timeout=23)
+        res = res | dict(status=r.status_code, url=r.url, headers=dict(r.headers), body=r.text)
+    except:
+        res = res | dict(status=10101, trace=traceback.format_exc())
+    return jsonify(res)
 
-@app.route('/about')
-def about():
-    return 'About v3'
