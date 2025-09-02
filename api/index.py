dacab5ecf8ddbfc444d6127470791bafef6685f6
S.M.Shahriar Zarir <148766170+zarir94@users.noreply.github.com>
2025-09-02 15:56:11 +0000

    fix

--- api/index.py
+++ api/index.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     url = request.args.get('url')
+    if not url:
+        return 'Add ?url=<any url>'
     res = dict(given=dict(url=url))
     try:
         r = get(url, timeout=23)
